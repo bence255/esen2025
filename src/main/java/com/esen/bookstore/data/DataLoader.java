58ca0632535604399f9559da984fd1844d82ab6c
Kerekes Bence <162814033+bence255@users.noreply.github.com>
2025-04-22 09:36:06 +0200

    Start implementing shell commands

--- src/main/java/com/esen/bookstore/data/DataLoader.java
+++ src/main/java/com/esen/bookstore/data/DataLoader.java
@@ -2,6 +2,7 @@ package com.esen.bookstore.data;
 
 
 import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.BookStore;
 import com.esen.bookstore.repository.BookRepository;
 import com.esen.bookstore.repository.BookStoreRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,19 +18,17 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DataLoader {
 
     private final BookRepository bookRepository;
     private final BookStoreRepository bookStoreRepository;
-
-    public DataLoader(BookRepository bookRepository, BookStoreRepository bookStoreRepository) {
-        this.bookRepository = bookRepository;
-        this.bookStoreRepository = bookStoreRepository;
-    }
 
     @Value("classpath:data/books.json")
     private Resource booksResource;
@@ -45,6 +44,20 @@ public class DataLoader {
             var booksJson = StreamUtils.copyToString(booksResource.getInputStream(), StandardCharsets.UTF_8);
             var books = objectMapper.readValue(booksJson, new TypeReference<List<Book>>(){});
             bookRepository.saveAll(books);
+
+            var bookStoreJson = StreamUtils.copyToString(bookstoresResource.getInputStream(), StandardCharsets.UTF_8);
+            var bookStores = objectMapper.readValue(bookStoreJson, new TypeReference<List<BookStore>>() {});
+
+            bookStores.forEach(bookStore -> bookStore.setInventory(books.stream()
+                        .collect(Collectors.toMap(
+                                book -> book,
+                                book -> ThreadLocalRandom.current().nextInt(1,50))
+                        )
+                ));
+
+            bookStoreRepository.saveAll(bookStores);
+
+            log.info("successfully loaded entities to the database");
         } catch (IOException e) {
             System.out.println("Cannot seed database" + e);
         }
